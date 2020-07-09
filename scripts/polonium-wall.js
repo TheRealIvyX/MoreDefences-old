96cbf156185e272f23573d0edb7846706dc228e8
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-07-09 14:54:52 +0200

    Create polonium-wall.js

---
+++ scripts/polonium-wall.js
@@ -0,0 +1,10 @@
+update(tile){
+  var r = 9 * Vars.tilesize;
+  Units.nearbyEnemies(tile.getTeam(), tile.drawx() - r, tile.drawy() - r, 2 * r, 2 * r, cons(unit => {
+    if(unit instanceof HealthTrait && !unit.isDead() && unit.withinDst(tile.drawx(), tile.drawy(), r)){
+      if (tile.name == 'Copper Wall') { // placeholder
+        unit.damage(0.333);
+      }
+    }
+  }));
+}
