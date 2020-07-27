c81788134c1b2b8a699c430e239311e9e9945e40
7rodo <7rodovje7878@gmail.com>
2020-07-27 11:54:57 +0100

    Update tefium-wall.js

--- scripts/tefium-wall.js
+++ scripts/tefium-wall.js
@@ -1,13 +1,13 @@
-const tefiumwallprojectile = extend(BasicBulletType, {
+const tBullet = extend(BasicBulletType, {
    draw(b){}
 });
-tefiumwallprojectile.speed = 6
-tefiumwallprojectile.damage = 60
-tefiumwallprojectile.lifetime = 60
-tefiumwallprojectile.homingPower = 0.05
-tefiumwallprojectile.homingRange = 75
-tefiumwallprojectile.frontColor = '#ffffff'
-tefiumwallprojectile.backColor = '#00875a'
+tBullet.speed = 6
+tBullet.damage = 60
+tBullet.lifetime = 60
+tBullet.homingPower = 0.05
+tBullet.homingRange = 75
+tBullet.frontColor = '#ffffff'
+tBullet.backColor = '#00875a'
 const tefiumwall = extendContent(Wall, "tefium-wall", {
   load() {
     this.bulletTimer = 0
@@ -18,7 +18,7 @@ const tefiumwall = extendContent(Wall, "tefium-wall", {
     if (this.bulletTimer > 120) {
       this.bulletTimer = 0
       for (var i = 0; i < 5; i++) {
-        Calls.createBullet(Bullets.tefiumwallprojectile, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+        blib.spawnBullet(tBullet, this.getTeam(), this.x, this.y)
       }
     }
   },
