76931155c76876b30a5fc5270838de4a4daadfef
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-07-27 10:38:51 +0200

    Update tefium-wall.js

--- scripts/tefium-wall.js
+++ scripts/tefium-wall.js
@@ -1,13 +1,13 @@
-const projectile = extend(BasicBulletType, {
+const tefiumwallprojectile = extend(BasicBulletType, {
    draw(b){}
 });
-projectile.speed = 6
-projectile.damage = 60
-projectile.lifetime = 60
-projectile.homingPower = 0.05
-projectile.homingRange = 75
-projectile.frontColor = '#ffffff'
-projectile.backColor = '#00875a'
+tefiumwallprojectile.speed = 6
+tefiumwallprojectile.damage = 60
+tefiumwallprojectile.lifetime = 60
+tefiumwallprojectile.homingPower = 0.05
+tefiumwallprojectile.homingRange = 75
+tefiumwallprojectile.frontColor = '#ffffff'
+tefiumwallprojectile.backColor = '#00875a'
 const tefiumwall = extendContent(Wall, "tefium-wall", {
   load() {
     this.bulletTimer = 0
@@ -18,7 +18,7 @@ const tefiumwall = extendContent(Wall, "tefium-wall", {
     if (this.bulletTimer > 120) {
       this.bulletTimer = 0
       for (var i = 0; i < 5; i++) {
-        Calls.createBullet(projectile, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+        Calls.createBullet(Bullets.tefiumwallprojectile, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
       }
     }
   },
