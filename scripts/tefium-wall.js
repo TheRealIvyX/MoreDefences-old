d719d7629dbd9dfbd10a589f790653b6a9a865a9
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-07-27 10:31:46 +0200

    Update tefium-wall.js

--- scripts/tefium-wall.js
+++ scripts/tefium-wall.js
@@ -9,14 +9,17 @@ projectile.homingRange = 75
 projectile.frontColor = '#ffffff'
 projectile.backColor = '#00875a'
 const tefiumwall = extendContent(Wall, "tefium-wall", {
-	update(tile){
-		this.super$update(tile);
-		this.bulletTimer++
+  load() {
+    this.bulletTimer = 0
+  },
+  update(tile){
+    this.super$update(tile);
+    this.bulletTimer++
     if (this.bulletTimer > 120) {
       this.bulletTimer = 0
       for (var i = 0; i < 5; i++) {
         Calls.createBullet(projectile, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
       }
     }
-	},
+  },
 });
