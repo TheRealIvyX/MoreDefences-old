afbe0e76436a3ae06983eb68d9c2d230ad49d71a
7rodo <7rodovje7878@gmail.com>
2020-07-27 12:59:09 +0100

    Update tefium-wall.js

--- scripts/tefium-wall.js
+++ scripts/tefium-wall.js
@@ -1,14 +1,12 @@
 const tefiumWallProjectile = extend(MissileBulletType, {
-   draw(b){}
 });
 tefiumWallProjectile.speed = 6
 tefiumWallProjectile.damage = 60
 tefiumWallProjectile.lifetime = 60
 tefiumWallProjectile.homingPower = 0.05
 tefiumWallProjectile.homingRange = 75
-tefiumWallProjectile.frontColor = '#ffffff'
-tefiumWallProjectile.backColor = '#00875a'
-
+tefiumWallProjectile.frontColor = Color.valueOf('#ffffff')
+tefiumWallProjectile.backColor = Color.valueOf('#00875a')
 const tefiumwall = extendContent(Wall, "tefium-wall", {
    load(){
     this.super$load();
@@ -19,8 +17,8 @@ const tefiumwall = extendContent(Wall, "tefium-wall", {
     this.super$update(tile);
     if (tile.entity.timer.get(this.shootTefiumWallProjectile, 120)){
      Calls.createBullet(tefiumWallProjectile, this.getTeam(), this.x, this.y, this.rotation, 1, 1)
-}
+     print('it works');
     }
-  },
+  }
 });
 tefiumWall.shootTefiumWallProjectile = tefium.timers++;
