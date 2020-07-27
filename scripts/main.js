b89b422bf1fc17cdcecba170379768fa14f0a171
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2020-07-27 10:25:26 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 require("more-defences-ivyx/vanadium-extractor")
 require("more-defences-ivyx/vanadium-megaextractor")
 // require("more-defences-ivyx/polonium-wall")
+require("more-defences-ivyx/tefium-wall")
